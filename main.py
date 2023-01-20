4b25e06e62b43a6075626eac1836d165a6f8dd92
Biencredible <fabian.bien@gmx.de>
2023-01-20 09:53:07 +0100

    - Draw gun - Rotate Gun with mouse - Lasers when clicking

--- main.py
+++ main.py
@@ -11,13 +11,38 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 backgrounds = []
 banners = []
 guns = []
-level = 1
+level = 3
 banner_top = HEIGHT - 200
 
 for level_index in range(1, 4):
     backgrounds.append(pygame.image.load(f'assets/bgs/{level_index}.png')) #f for formatted string because a variable is needed. Access variable with {level_index}
     banners.append(pygame.image.load(f'assets/banners/{level_index}.png'))
-    guns.append(pygame.image.load(f'assets/guns/{level_index}.png'))
+    guns.append(pygame.transform.scale(pygame.image.load(f'assets/guns/{level_index}.png'), (100, 100)))
+
+def draw_gun():
+    mouse_pos = pygame.mouse.get_pos()
+    gun_point = (WIDTH/2, banner_top)
+    lasers = ['red', 'purple', 'green']
+    clicks = pygame.mouse.get_pressed()
+    if mouse_pos[0] != gun_point[0]:
+        slope = (mouse_pos[1] - gun_point[1])/(mouse_pos[0] - gun_point[0])
+    else:
+        slope = -100000 #large negative number is approx straight line
+    angle = math.atan(slope)
+    rotation = math.degrees(angle)
+    if mouse_pos[0] < WIDTH/2:
+        gun = pygame.transform.flip(guns[level - 1], True, False)
+        if mouse_pos[1] < banner_top:
+            screen.blit(pygame.transform.rotate(gun, 90 - rotation), (WIDTH/2 - 90, HEIGHT - 250))
+            if clicks[0]:
+                pygame.draw.circle(screen, lasers[level -1], mouse_pos, 5)
+    else:
+        gun = guns[level -1]
+        if mouse_pos[1] < banner_top:
+            screen.blit(pygame.transform.rotate(gun, 270 - rotation), (WIDTH/2 - 30, HEIGHT - 250))
+            if clicks[0]:
+                pygame.draw.circle(screen, lasers[level -1], mouse_pos, 5)
+
 
 #main loop
 run = True
@@ -28,7 +53,8 @@ while run:
     screen.blit(backgrounds[level - 1], (0, 0))
     screen.blit(banners[level - 1], (0, banner_top))
 
-    #if level > 0:
+    if level > 0:
+        draw_gun()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
